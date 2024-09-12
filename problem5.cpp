3f5b12c3c8769ce6ad3224bde2aad3032abe7cc9
azizbek117 <raximovazizbek601@gmail.com>
2024-09-12 14:17:45 +0500

    Update problem5.cpp

--- problem5.cpp
+++ problem5.cpp
@@ -1,16 +1,16 @@
+
 #include <iostream>
 
  using namespace std;
  int main() {
      float s;
      float m;
-     cin >> m,s;
-     if (m*30.48>s) {
-         cout <<1<< endl;
+     cin >>m,s;
+     if (m*30.48<s) {
+         cout <<0;
      }
      else{
-         cout<<0<<endl;
+         cout<<1;
      }
      return 0;
  }
-
