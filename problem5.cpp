2337f3d826b90f43dff8f2ccfa475c7d75797579
azizbek117 <raximovazizbek601@gmail.com>
2024-09-12 14:03:59 +0500

    Update problem5.cpp

--- problem5.cpp
+++ problem5.cpp
@@ -1,18 +1,14 @@
-//
-// Created by AKTIV on 11
-
 #include <iostream>
 
  using namespace std;
  int main() {
      float s;
      float m;
-     cin >> s,m;
+     cin >> m,s;
      if(m*30.48>s) {
          cout<<1<<endl;
-     } else {
-         cout<<0<<endl;
-     }
+     
+  }
      return 0;
 
  }
